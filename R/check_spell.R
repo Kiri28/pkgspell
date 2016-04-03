dcecff352ec4c1dde486ab043abb2c5c462492f7
irudnyts <yegor.rudnytskiy@gmail.com>
2016-04-03 16:41:35 +0200

    Add check_descr function, which checks DESCRIPTION file

--- R/check_spell.R
+++ R/check_spell.R
@@ -1,4 +1,40 @@
-check_spell <- function(path, descr = T) {
+check_spell <- function(path) {
+
+    if(!dir.exists(path)) {
+        stop(paste("Directory", path, "does not exist."))
+    }
     typos <- data.frame(File = character(0), Line = integer(),
-                        Work = character(0), Suggestion = character())
+                        Word = character(0), stringsAsFactors = F)
+
+    #---- check DESCRIPTION file
+
+
+    return(typos)
+}
+
+# NOTE: DESCRIPTION has to have Title: and Description:
+# each new line in Description: filed should be intended by 4 spaces
+check_descr <- function(dfile) {
+    if(!file.exists(dfile)) {
+        stop("File DESCRIPTION does not exist.")
+    }
+    typos <- data.frame(File = character(0), Line = integer(),
+                        Word = character(0), stringsAsFactors = F)
+    fields <- c("Title:", "Description:")
+    text <- readLines(con = dfile)
+    for(field in fields) {
+        line <- grep(field, text)
+        is_curr_field <- length(line) == 1
+        while(is_curr_field) {
+            bad_words <- hunspell::hunspell(text[line])[[1]]
+            for(word in bad_words) {
+                typos <- rbind(typos, data.frame(File = "DESCRIPTION",
+                                                 Line = line, Word = word,
+                                                 stringsAsFactors = F))
+            }
+            line <- line + 1
+            is_curr_field <- substr(text[line], start = 1, stop = 4) == "    "
+        }
+    }
+    return(typos)
 }
